5f9523e6144b00f5d802ca06b116c73ed1585a17
llambda <xxgsoftware@gmail.com>
2015-05-02 11:40:05 -0500

    sending messages

--- client/main.js
+++ client/main.js
@@ -12,6 +12,8 @@ Bro.controller = function () {
   this.bros = m.prop([])
   this.phonenumberapi = m.prop('')
   this.noauth = function () { return self.phonenumberapi() == '' }
+  this.to = m.prop('')
+  this.message = m.prop('')
 
   this.whoami = function () {
     m.request({url:'/api/registration/phone'})
@@ -31,6 +33,10 @@ Bro.controller = function () {
   this.broMe = function () {
     m.request({method: 'POST', url: '/api/bro', data: { to: self.phonenumber(), text: 'sup bro!'} })
   }
+  
+  this.send = function () {
+    m.request({method: 'POST', url: '/api/bro', data: { to: self.to(), text: self.message() } })
+  }
 
   this.getBros = function () {
     m.request({method: 'GET', url: '/api/bro'}).then(self.bros)
@@ -42,10 +48,21 @@ Bro.view = function (ctrl) {
       m('label', 'Phone number'),
       m('input', {oninput: m.withAttr('value', ctrl.phonenumber) }),
       m('button', {onclick: ctrl.loginClick},'Login'),
+      m('span', 'logged in as ' + ctrl.phonenumberapi()),
     ]),
-    m('div', 'logged in as ' + ctrl.phonenumberapi()),
-    m('button', {onclick: ctrl.broMe, disabled: ctrl.noauth() },'Bro Myself!'),
-    m('button', {onclick: ctrl.getBros, disabled: ctrl.noauth() },'Get messages!'),
+    
+    m('br'),
+    
+    m('div', [
+      m('label', 'To: '), m('br'),
+      m('input', {oninput: m.withAttr('value', ctrl.to) }),m('br'),
+      m('label', 'Msg: '),m('br'),
+      m('input', {oninput: m.withAttr('value', ctrl.message) }), m('br'),
+      m('button', {onclick: ctrl.send, disabled: ctrl.noauth() }, 'Send Bro!'),
+    ]),
+  	m('br'),
+    m('button', {onclick: ctrl.broMe, disabled: ctrl.noauth() }, 'Bro Myself!'),
+    m('button', {onclick: ctrl.getBros, disabled: ctrl.noauth() }, 'Get messages!'),
     m('div', ctrl.bros().map(function (bro) {
       return [m('label', 'From: '), m('span', bro.from), m('br'),
       m('label', 'Date: '), m('span', moment(bro.date).fromNow()), m('br'),
