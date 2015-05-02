b42da92770883dc604496b02e0bd6420abd38336
llambda <xxgsoftware@gmail.com>
2015-05-02 09:27:53 -0500

    add login

--- client/main.js
+++ client/main.js
@@ -5,15 +5,16 @@ regsw()
 var Bro = {}
 Bro.controller = function () {
   var self = this
-  this.phone = m.prop('')
+  this.phonenumber = m.prop('')
   this.loginClick = function () {
-    console.log(self.phone())
+    console.log(self.phonenumber())
+    m.request({method: 'POST', url: '/api/registration/phone', data: { phonenumber: self.phonenumber() } })
   }
 }
 Bro.view = function (ctrl) {
   return [
     m('label', 'Phone number'),
-    m('input', {oninput: m.withAttr('value', ctrl.phone) }),
+    m('input', {oninput: m.withAttr('value', ctrl.phonenumber) }),
     m('button', {onclick: ctrl.loginClick},'Login'),
     m('div', 'bro!')
   ]
