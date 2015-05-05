80b40b7ab68be8d20163d752e27844e053ee4992
llambda <xxgsoftware@gmail.com>
2015-05-04 19:23:08 -0500

    message posting from serviceworker

--- client/main.js
+++ client/main.js
@@ -1,5 +1,11 @@
 'use strict';
 
+window.addEventListener("message", receiveMessage, false);
+
+function receiveMessage(message) {
+	console.log(message);
+}
+
 var m = require('mithril')
 var moment = require('moment')
 var regsw = require('./regsw')
--- client/sw.js
+++ client/sw.js
@@ -6,6 +6,20 @@ self.addEventListener('push', function(event) {
   //  var icon = '/images/icon-192x192.png';
   var tag = 'simple-push-demo-notification-tag';
 
+
+  clients.matchAll({includeUncontrolled: true}).then(function(clients) {
+    console.log('outer clients' )
+    console.log(clients)
+    for (var i = 0; i < clients.length; i++) {
+      var client = clients[i];
+      console.log('clients ' + i)
+      console.log(client)
+      if (client['postMesssage']) {
+        client['postMesssage']('yourmessage');
+      }
+    }
+  });
+
   event.waitUntil(
     self.registration.showNotification(title, {
       body: body,
