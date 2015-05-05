50e510a79b12b1b3904f565691b676d708247746
llambda <xxgsoftware@gmail.com>
2015-05-04 21:00:18 -0500

    better deploys

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-scp -r app.sh client lib package.json server.js joe@104.237.135.61:yobro
+npm run build
+mkdir dist
+mkdir dist/client
+cp -R lib package.json server.js dist
+cp client/bundle.js dist/client
+cp client/index.html dist/client
+cp client/manifest.json dist/client
+cp client/sw.js dist/client
+scp -r dist joe@104.237.135.61:yobro
