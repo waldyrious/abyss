c976c516a9ba9535b6a2127b23203a756f010a32
llambda <xxgsoftware@gmail.com>
2015-05-04 20:13:02 -0500

    better var name

--- client/login.js
+++ client/login.js
@@ -5,7 +5,7 @@ var styler = require('./styler');
 module.exports.controller = function (args, extras) {
 	var self = this
 	 
-	this.phonenumber = m.prop('')
+	this.phoneInput = m.prop('')
 	this.phonenumberapi = m.prop('')
 	this.whoami = function () {
 	    m.request({url:'/api/registration/phone'})
@@ -14,7 +14,7 @@ module.exports.controller = function (args, extras) {
 	this.noauth = function () { return self.phonenumberapi() == '' }
 	this.loginClick = function () {
 		return m.request({method: 'POST',
-		 url: '/api/registration/phone', data: { phonenumber: self.phonenumber() } })
+		 url: '/api/registration/phone', data: { phonenumber: self.phoneInput() } })
 		.then(function (response) {
 			self.phonenumberapi(response)
 		})
@@ -26,7 +26,7 @@ module.exports.view = function (ctrl) {
 	return m('div', [
 		m('div', 'Logged in as: ' + ctrl.phonenumberapi()),
 		m('label', 'Phone number: '),
-		m('input', {oninput: m.withAttr('value', ctrl.phonenumber) }),
+		m('input', {oninput: m.withAttr('value', ctrl.phoneInput) }),
 		m('button', styler.buttonify({onclick: ctrl.loginClick}), 'Login'),
 
 		m.component(Bro, {
