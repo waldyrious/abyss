d224773d407fd75af3736c92edbb75210f112475
llambda <xxgsoftware@gmail.com>
2015-05-06 21:45:40 -0500

    fix styler

--- client/styler.js
+++ client/styler.js
@@ -1,18 +1,21 @@
 var _ = require('lodash');
 
 module.exports.buttonify = function (obj) {
+	if (!obj) obj = {};
 	return _.merge(obj, {
     	class: "btn btn-default"
     });
 }
 
 module.exports.bbuttonify = function (obj) {
+	if (!obj) obj = {};
     return _.merge(obj, {
     	class: "btn btn-default btn-lg btn-primary"
     });
 }
 
 module.exports.pointer = function (obj) {
+	if (!obj) obj = {};
 	return _.merge(obj, {
 		style: {
 			cursor:'pointer'
