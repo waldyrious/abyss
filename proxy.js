5db3f71f1c8c225d685062188e37b33d37ccf2f0
llambda <xxgsoftware@gmail.com>
2015-05-08 23:01:04 -0500

    add proxy for rethinkdb

---
+++ proxy.js
@@ -0,0 +1,17 @@
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer();
+var http = require('http');
+var fs = require('fs');
+var auth = require('./secret/proxyauth.json').auth;
+console.log(auth)
+
+http.createServer(function (req, res) {
+  if (!req.headers['authorization'] || req.headers['authorization'] !== auth) {
+    res.writeHead(401, {
+      'WWW-Authenticate': 'Basic realm="users"'
+    });
+    res.end();
+  } else 
+    proxy.web(req, res, { target: 'http://localhost:8080/' });
+}).listen(8000);
+
