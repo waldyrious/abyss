9fbf7d1e6926d64f33a8ad680b78d3c19cae2ac5
llambda <xxgsoftware@gmail.com>
2015-05-12 20:43:10 -0500

    Add trimming and error handling on the login

--- client/login.js
+++ client/login.js
@@ -1,10 +1,32 @@
 var m = require('mithril');
 var messages = require('./messages');
 var styler = require('./styler');
+var _ = require('lodash');
 
 module.exports.controller = function (args, extras) {
 	var self = this
-	 
+	
+	this.error = (function() {
+		var error = null;
+		var timeout;
+
+		return function (value) {
+			if (value) {
+				error = value;
+
+				if (timeout) {
+					clearTimeout(timeout);
+				}
+				timeout = setTimeout(function () {
+					error = null;
+					m.redraw();
+				}.bind(this), 4000);
+			} else {
+				return error;
+			}
+		}.bind(this);
+	})();
+
 	this.phoneInput = m.prop('')
 	this.needCode = m.prop(false)
 	this.codeInput = m.prop('')
@@ -21,31 +43,46 @@ module.exports.controller = function (args, extras) {
 		.then(function (response) {
 			self.phonenumberapi('')
 			self.codeInput('')
-		})
+		}, self.error)
 	}
 	this.whoami = function () {
 	    m.request({url:'/api/registration/phone'})
-	    .then(self.phonenumberapi)
+	    .then(self.phonenumberapi, self.error)
 	  }
 	this.noauth = function () { return self.phonenumberapi() == '' }
 	this.loginClick = function () {
 		return m.request({method: 'POST',
-		 url: '/api/registration/phone', data: { phonenumber: self.phoneInput() } })
+		 url: '/api/registration/phone', data: { phonenumber: self.phoneInput().trim() } })
 		.then(function (response) {
 			self.needCode(true)
 			self.codeInput('')
-		})
+		}, self.error)		
 	}
 	this.submitCode = function () {
 		return m.request({method: 'POST',
-		 url: '/api/registration/code', data: { code: self.codeInput() } })
+		 url: '/api/registration/code', data: { code: self.codeInput().trim() } })
 		.then(function (response) {
 			self.phonenumberapi(response)
 			self.needCode(false)
 			self.codeInput('')
-		})
+		}, self.error)
 	}
 	this.whoami()
+}
+
+function renderError(error) {
+	if (!error) return null;
+
+	var value;
+
+	if (_.isObject(error())) {
+		if (error().error && error().error.text)
+			value = error().error.text;
+	} else {
+		value = error();
+	}
+
+	return m('div', {class:'bg-danger'}, value);
 }
 
 module.exports.view = function (ctrl) {
@@ -53,6 +90,7 @@ module.exports.view = function (ctrl) {
 	if (ctrl.noauth()) {
 		if (ctrl.needCode()) {
 			return m('div', [
+				renderError(ctrl.error),
 				m('div', 'Enter verification code: '),
 				m('input', {type:'tel', oninput: m.withAttr('value', ctrl.codeInput), value: ctrl.codeInput()}),
 				m('button', styler.buttonify({onclick: ctrl.submitCode}), 'Submit Code'),
@@ -60,6 +98,7 @@ module.exports.view = function (ctrl) {
 			])
 		} else {
 			return m('div', [
+				renderError(ctrl.error),
 				m('div', 'Log in with your phone number!' + ctrl.phonenumberapi()),
 				m('input', {type:'tel', oninput: m.withAttr('value', ctrl.phoneInput), value: ctrl.phoneInput()}),
 				m('button', styler.buttonify({onclick: ctrl.loginClick}), 'Login'),
