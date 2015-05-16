8fe0519ae724ba75efef863c7bceab21a627e169
llambda <xxgsoftware@gmail.com>
2015-05-16 12:51:58 -0500

    Watchify within runner.js

--- runner.js
+++ runner.js
@@ -1,6 +1,6 @@
-
+'use strict';
 // Run this to auto-reload the server on file changes.
-
+var Promise = require('bluebird');
 var nodemon = require('nodemon');
 
 nodemon({
@@ -8,12 +8,40 @@ nodemon({
 	ext: 'js json',
 	"execMap": {
 		"js": "node --harmony"
-	}
+	},
+	"ignore": [
+		".git",
+		"client/**"
+	]
 });
 
-//var browserify = require('browserify');
-//var watchify = require('watchify');
-//var fromArgs = require('watchify/bin/args');
-//
-//var b = browserify({ cache: {}, packageCache: {} });
-//var w = watchify(b);
+var watchify = require('watchify');
+var browserify = require('browserify');
+var vm = require('vm');
+
+var fs = Promise.promisifyAll(require('fs'));
+var path = require('path');
+
+var file = path.join(__dirname +'/client/main.js');
+
+var lastWrite = Promise.resolve(true);
+
+var w = watchify(browserify(file, watchify.args));
+w.on('update', function () {
+	w.bundle(function (err, src) {
+		if (err) {
+			console.error(err);
+			w.close();
+		} else if (src) {
+			lastWrite.then(function () {
+				console.log('Rebundling front end');
+				lastWrite = fs.writeFileAsync(__dirname + '/client/bundle.js', src)
+				.then(function () {
+					console.log('done');
+				})
+			})
+		}
+	});
+});
+
+w.emit('update'); // trigger initial build
