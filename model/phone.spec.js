1629cd39db388aa71435116d8c48dfbc7135ae0f
llambda <xxgsoftware@gmail.com>
2015-05-16 15:03:26 -0500

    move spec files next to files

--- model/phone.spec.js
+++ model/phone.spec.js
@@ -2,20 +2,18 @@
 
 var test = require('tape');
 
-var Phone = require('../model/phone')
+var Phone = require('./phone');
 
 test('shouldnt need new', function (t) {
 	t.plan(1);
 
 	var good = Phone('+1555-555-5555');
 	t.equal(good.strip(), '15555555555');
-	
-})
+});
 
 test('should', function (t) {
 	t.plan(1);
 
 	var good = new Phone('+1555-555-5555');
 	t.equal(good.strip(), '15555555555');
-	
-})
+});
--- runner.js
+++ runner.js
@@ -11,7 +11,8 @@ nodemon({
 	},
 	"ignore": [
 		".git",
-		"client/**"
+		"client/**",
+		"**/*.spec.js"
 	]
 });
 
