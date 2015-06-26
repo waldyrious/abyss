b694c117262b580f1371bb5a15afa6ee9272b504
llambda <xxgsoftware@gmail.com>
2015-06-26 18:20:57 -0500

    Fix svg loader double declaed, fixes bootstrap glyphicons.

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,6 @@ module.exports = {
             {test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
             {test: /\.less$/, loader: 'style-loader!css-loader!less-loader'}, // use ! to chain loaders
             {test: /\.css$/, loader: 'style-loader!css-loader'},
-            {test: /\.svg$/, loader: 'svg-loader'},
             {test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
             {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
             {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
