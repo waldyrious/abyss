767e9752dad880caeb3690c1dbc5060f681f2d8e
llambda <xxgsoftware@gmail.com>
2015-06-27 10:39:56 -0500

    Beautified

--- app/messages.js
+++ app/messages.js
@@ -19,7 +19,7 @@ var union = require('lodash/array/union');
 var merge = require('lodash/object/merge');
 
 
-module.exports.controller = function (args, extras) {
+module.exports.controller = function(args, extras) {
 	var self = this;
 	self.working = m.prop(false);
 
@@ -32,36 +32,36 @@ module.exports.controller = function (args, extras) {
 
 	var withAuth = function(xhr) {
 		if (args.jwt()) {
-    		xhr.setRequestHeader('Authorization', 'Bearer ' + args.jwt());
+			xhr.setRequestHeader('Authorization', 'Bearer ' + args.jwt());
 		}
 	}
 
-	var oboeAuth = function () {
+	var oboeAuth = function() {
 		return {
-			'Authorization':  'Bearer ' + args.jwt()
+			'Authorization': 'Bearer ' + args.jwt()
 		}
 	}
 
-	self.getNickname = function (ph) {
+	self.getNickname = function(ph) {
 		if (self.nicknames[ph] !== undefined) {
 			return self.nicknames[ph];
-		} else if (ph === args.me().id){
+		} else if (ph === args.me().id) {
 			return args.me().nickname;
 		} else {
 			return null;
 		}
 	}
 
-	self.selectGroup = function (group) {
+	self.selectGroup = function(group) {
 		self.to = clone(group);
 		self.getMessagesStreaming();
 	};
 
-	self.reselectGroup = function () {
+	self.reselectGroup = function() {
 		self.getMessagesStreaming();
 	};
 
-	self.selectFirstGroup = function () {
+	self.selectFirstGroup = function() {
 		if (isEqual(self.to, ['']) && self.conversations[0]) {
 			self.to = clone(self.conversations[0].group);
 			self.getMessagesStreaming();
@@ -74,17 +74,17 @@ module.exports.controller = function (args, extras) {
 
 	function immediate(fn) {
 		m.startComputation();
-		setImmediate(function () {
+		setImmediate(function() {
 			fn();
 			m.endComputation();
 		});
 	}
 
-	self.setMessages = function (value) {
+	self.setMessages = function(value) {
 		self.messages = value;
 	}
 
-	self.setConversations = function (value) {
+	self.setConversations = function(value) {
 		self.conversations = value.groupings;
 		self.nicknames = value.nicknames;
 
@@ -95,12 +95,12 @@ module.exports.controller = function (args, extras) {
 		return !fromMe(message) && message.to.length && message.to.length > 1;
 	}
 
-	self.toPlus = function () {
+	self.toPlus = function() {
 		self.to.push('');
 		self.reselectGroup();
 	};
 
-	self.toMinus = function () {
+	self.toMinus = function() {
 		self.to.pop();
 
 		if (self.to.length === 0) {
@@ -110,32 +110,50 @@ module.exports.controller = function (args, extras) {
 		self.reselectGroup();
 	};
 
-	self.send = function () {
+	self.send = function() {
 		self.working(true);
-		self.to = filter(self.to, function (item) {
+		self.to = filter(self.to, function(item) {
 			return item !== '' && item !== ' ' && item !== null;
 		});
-		m.request({method: 'POST', config: withAuth, background: false, url: '/api/messages', data: {to: self.to, text: self.message()}})
-		.then(function () {
-			self.message('');
-			self.working(true);
-		})
-		.then(self.refresh, self.error)
+		m.request({
+				method: 'POST',
+				config: withAuth,
+				background: false,
+				url: '/api/messages',
+				data: {
+					to: self.to,
+					text: self.message()
+				}
+			})
+			.then(function() {
+				self.message('');
+				self.working(true);
+			})
+			.then(self.refresh, self.error)
 	};
 
-	self.getMessages = function () {
-		m.request({method: 'GET', config: withAuth, url: '/api/messages?group=' + encodeURIComponent(JSON.stringify(self.to))})
-		.then(self.setMessages, self.error)
+	self.getMessages = function() {
+		m.request({
+				method: 'GET',
+				config: withAuth,
+				url: '/api/messages?group=' + encodeURIComponent(JSON.stringify(self.to))
+			})
+			.then(self.setMessages, self.error)
 	};
 
-	self.refresh = self.getConversations = function () {
+	self.refresh = self.getConversations = function() {
 		self.working(true);
-		m.request({method: 'GET', config: withAuth, background: false, url: '/api/conversations'})
-		.then(self.setConversations, self.error)
-		.then(self.getMessagesStreaming, self.error)
+		m.request({
+				method: 'GET',
+				config: withAuth,
+				background: false,
+				url: '/api/conversations'
+			})
+			.then(self.setConversations, self.error)
+			.then(self.getMessagesStreaming, self.error)
 	};
 
-	self.getMessagesStreaming = function () {
+	self.getMessagesStreaming = function() {
 		// Stream in first 10 messages and try to render them ASAP as we load the rest
 		var count = 0;
 		var show = 9;
@@ -143,57 +161,71 @@ module.exports.controller = function (args, extras) {
 		m.startComputation();
 		self.messages = [];
 		oboe({
-			url: '/api/messages?group=' + encodeURIComponent(JSON.stringify(self.to)),
-			headers: oboeAuth()
-		}).node('![*]', function (item) {
-			self.messages.push(item);
-			count++;
-			if (count == show) {
+				url: '/api/messages?group=' + encodeURIComponent(JSON.stringify(self.to)),
+				headers: oboeAuth()
+			}).node('![*]', function(item) {
+				self.messages.push(item);
+				count++;
+				if (count == show) {
+					m.endComputation();
+					m.startComputation();
+				}
+				return oboe.drop;
+			})
+			.done(function() {
+				self.working(false);
 				m.endComputation();
-				m.startComputation();
-			}
-			return oboe.drop;
-		})
-		.done(function () {
-			self.working(false);
-			m.endComputation();
-		});
+			});
 	};
 
-	self.clearMessages = function () {
+	self.clearMessages = function() {
 		self.working(true);
-		m.request({method: 'DELETE', config: withAuth, background: false, url: '/api/messages'})
-		.then(self.refresh, self.error)
+		m.request({
+				method: 'DELETE',
+				config: withAuth,
+				background: false,
+				url: '/api/messages'
+			})
+			.then(self.refresh, self.error)
 	};
 
-	self.delete = function (message) {
+	self.delete = function(message) {
 		self.working(true);
-		m.request({method: 'DELETE', config: withAuth, background: false, url: '/api/messages/' + encodeURIComponent(message.id)})
-		// .then(function () {
-		// 	self.messages.splice(self.messages.indexOf(message), 1);
-		// }, self.error);
-		.then(self.refresh, self.error)
+		m.request({
+				method: 'DELETE',
+				config: withAuth,
+				background: false,
+				url: '/api/messages/' + encodeURIComponent(message.id)
+			})
+			// .then(function () {
+			// 	self.messages.splice(self.messages.indexOf(message), 1);
+			// }, self.error);
+			.then(self.refresh, self.error)
 	};
 
 	self.refresh();
 };
 
-module.exports.view = function (ctrl, args, extras) {
+module.exports.view = function(ctrl, args, extras) {
 
-	var fadesIn = function (element, isInitialized, context) {
+	var fadesIn = function(element, isInitialized, context) {
 		if (!isInitialized) {
 			element.style.opacity = 0;
-			Velocity(element, {opacity: 1})
+			Velocity(element, {
+				opacity: 1
+			})
 		}
 	};
 
-	var fadesOut = function (callback) {
-		return function (e) {
+	var fadesOut = function(callback) {
+		return function(e) {
 			//don't redraw yet
 			m.redraw.strategy("none");
 
-			Velocity(e.target.parentNode, {opacity: 0}, {
-				complete: function () {
+			Velocity(e.target.parentNode, {
+				opacity: 0
+			}, {
+				complete: function() {
 					//now that the animation finished, redraw
 					m.startComputation();
 					callback();
@@ -220,70 +252,79 @@ module.exports.view = function (ctrl, args, extras) {
 
 	function displayMessage(message) {
 		return m('.media', {
-			key: message.id,
-			config: fadesIn
-		},
+				key: message.id,
+				config: fadesIn
+			},
 
-		[m('.media-body',
-			[m('.media-heading', 'From: ',
-				m('b', fromMe(message)? (args.me().nickname ? args.me().nickname : 'me') : message.from + (ctrl.getNickname(message.from) ? ' (' + ctrl.getNickname(message.from) + ')' : '')),
-				m('span', ' ' + moment(message.date).fromNow()),
-				' ', m('button.btn btn-default glyphicon glyphicon-trash', {
-					onclick: fadesOut(ctrl.delete.bind(this, message))
-				})
-				)
-			]),
-			m.trust(autolinker.link(message.text)),
-			m('br'),
+			[m('.media-body', [m('.media-heading', 'From: ',
+					m('b', fromMe(message) ? (args.me().nickname ? args.me().nickname : 'me') : message.from + (ctrl.getNickname(message.from) ? ' (' + ctrl.getNickname(message.from) + ')' : '')),
+					m('span', ' ' + moment(message.date).fromNow()),
+					' ', m('button.btn btn-default glyphicon glyphicon-trash', {
+						onclick: fadesOut(ctrl.delete.bind(this, message))
+					})
+				)]),
+				m.trust(autolinker.link(message.text)),
+				m('br'),
 
-			m('hr')
-		]
+				m('hr')
+			]
 		)
 	}
 
-	return m('div', {config: fadesIn}, [
+	return m('div', {
+		config: fadesIn
+	}, [
 		m('div', [
 			error.renderError(ctrl.error),
 			m('label', 'To: '), m('span', ' '),
-			m('button.btn btn-default', {onclick: ctrl.toPlus}, '+'),
-			m('button.btn btn-default', {onclick: ctrl.toMinus}, '-'),
+			m('button.btn btn-default', {
+				onclick: ctrl.toPlus
+			}, '+'),
+			m('button.btn btn-default', {
+				onclick: ctrl.toMinus
+			}, '-'),
 			m('br'),
-			ctrl.to.map(function (item, index) {
+			ctrl.to.map(function(item, index) {
 				return m('input', {
 					placeholder: 'Phone number...',
-					type: 'tel', onchange: m.withAttr('value', function (value) {
+					type: 'tel',
+					onchange: m.withAttr('value', function(value) {
 						ctrl.to[index] = value
-					}), value: ctrl.to[index]
+					}),
+					value: ctrl.to[index]
 				})
 			}),
 			m('div.form-inline',
-			m('div.form-group', m('label', 'Message: '), m('br'),
-			m('input.form-control', {
-				placeholder: 'Message Text...',
-				onchange: m.withAttr('value', ctrl.message),
-				value: ctrl.message()
-			}),
+				m('div.form-group', m('label', 'Message: '), m('br'),
+					m('input.form-control', {
+						placeholder: 'Message Text...',
+						onchange: m.withAttr('value', ctrl.message),
+						value: ctrl.message()
+					}),
 
-			m('button.btn btn-default glyphicon glyphicon-refresh', {onclick: ctrl.refresh}),
-			' ',
-			m('button.btn btn-default btn-primary glyphicon glyphicon-send', {onclick: ctrl.send})))
+					m('button.btn btn-default glyphicon glyphicon-refresh', {
+						onclick: ctrl.refresh
+					}),
+					' ',
+					m('button.btn btn-default btn-primary glyphicon glyphicon-send', {
+						onclick: ctrl.send
+					})))
 		]),
 		// m('button', buttonify({onclick: ctrl.clearMessages}), 'Delete all messages!'),
-		m('div', [m('div.col-sm-4#left',
-		[m('h3', 'Conversations'),
-			ctrl.conversations.map(function (grouping) {
-				return m('div', styler.pointer(styler.round({
-					onclick: function () {
-						ctrl.selectGroup(grouping.group)
-					},
-					class: isEqual(flatten(grouping.group), ctrl.to) ? 'bg-info' : null
-				})),
-				[simplify(grouping.group).map(function (ph) {
-					return m('div', ph + (ctrl.getNickname(ph) ? ' (' + ctrl.getNickname(ph) + ')' :''))
-				}),
-					m('hr')
-				])
-			})]),
+		m('div', [m('div.col-sm-4#left', [m('h3', 'Conversations'),
+				ctrl.conversations.map(function(grouping) {
+					return m('div', styler.pointer(styler.round({
+						onclick: function() {
+							ctrl.selectGroup(grouping.group)
+						},
+						class: isEqual(flatten(grouping.group), ctrl.to) ? 'bg-info' : null
+					})), [simplify(grouping.group).map(function(ph) {
+							return m('div', ph + (ctrl.getNickname(ph) ? ' (' + ctrl.getNickname(ph) + ')' : ''))
+						}),
+						m('hr')
+					])
+				})
+			]),
 			m('div.col-sm-8#right', [m('h3', 'Messages'),
 				ctrl.messages.map(displayMessage)
 			])
