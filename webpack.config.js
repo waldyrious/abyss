558070004ade40a96c577ceb6f06c3c47a89b3dc
llambda <xxgsoftware@gmail.com>
2015-06-26 19:52:31 -0500

    Remove comment

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
         ],
         loaders: [
             {test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
-            {test: /\.less$/, loader: 'style-loader!css-loader!less-loader'}, // use ! to chain loaders
+            {test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},
             {test: /\.css$/, loader: 'style-loader!css-loader'},
             {test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
             {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
