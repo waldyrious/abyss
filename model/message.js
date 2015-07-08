1e2bef717d4d0ed87a3920802c4b7a3ce6214a06
llambda <xxgsoftware@gmail.com>
2015-07-07 21:52:46 -0500

    Remove deletedBy

--- model/message.js
+++ model/message.js
@@ -1,10 +1,9 @@
 var uuid = require('uuid');
 
-module.exports = function Message () {	
+module.exports = function Message () {
 	this.text = '';
 	this.from = '';
 	this.to = [];
 	this.id = uuid();
-	this.deletedBy = [];
 	this.date = new Date();
 };
