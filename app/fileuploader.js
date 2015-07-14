2950e6a356faeefca7d2bed9a2bd294475e5a29d
llambda <xxgsoftware@gmail.com>
2015-07-13 21:57:20 -0500

    Allow multiple files in upload.

--- app/fileuploader.js
+++ app/fileuploader.js
@@ -1,5 +1,6 @@
 'use strict';
 var m = require('mithril');
+var Promise = require('bluebird');
 
 module.exports.controller = function(args, extras) {
 	var self = this;
@@ -26,30 +27,31 @@ module.exports.controller = function(args, extras) {
 		self.uploadTotal(0);
 		self.uploaded(0);
 
-		var fileList = self.files();
+		return Promise.map(Array.prototype.slice.call(self.files()), function(file) {
+				var data = new FormData();
+				data.append("file", file);
 
-		var data = new FormData();
-		var file = fileList[0];
-
-		data.append("file", file);
-
-		var xhrConfig = function(xhr) {
-			xhr = withAuth(xhr);
-			xhr.upload.addEventListener("progress", function(ev) {
-				self.uploaded(ev.loaded);
-				self.uploadTotal(ev.total);
-				m.redraw();
-			});
-		}
-
-		m.request({
-				method: "POST",
-				url: '/api/file?group=' + encodeURIComponent(JSON.stringify(self.to)) + '&type=' + encodeURIComponent(file.type) + '&lastModified=' + encodeURIComponent(file.lastModified) + '&size=' + encodeURIComponent(file.size) + '&name=' + encodeURIComponent(file.name),
-				data: data,
-				config: xhrConfig,
-				serialize: function(data) {
-					return data
+				var xhrConfig = function(xhr) {
+					xhr = withAuth(xhr);
+					xhr.upload.addEventListener("progress", function(ev) {
+						self.uploaded(ev.loaded);
+						self.uploadTotal(ev.total);
+						m.redraw();
+					});
 				}
+
+				return m.request({
+					method: "POST",
+					url: '/api/file?group=' + encodeURIComponent(JSON.stringify(self.to)) + '&type=' + encodeURIComponent(file.type) + '&lastModified=' + encodeURIComponent(file.lastModified) + '&size=' + encodeURIComponent(file.size) + '&name=' + encodeURIComponent(file.name),
+					data: data,
+					config: xhrConfig,
+					serialize: function(data) {
+						return data
+					}
+				})
+				.then(args.refresh)
+			}, {
+				concurrency: 1
 			})
 			.then(function() {
 				//reset file input here
@@ -72,18 +74,25 @@ module.exports.view = function(ctrl, args, extras) {
 		sendButton = element;
 	}
 
-	return m('div', [
+	return m('div', {
+			style: {
+				'margin-top': '4px'
+			}
+	},[
 		m('button.btn btn-success glyphicon glyphicon-file', {
 			disabled: ctrl.files() ? false : true,
 			onclick: ctrl.uploadFile,
 			config: sendButtonConfig
-		}, ' Send file'),
-		ctrl.uploaded() ? m('span', ' ' + Math.trunc(ctrl.uploaded() / ctrl.uploadTotal() * 100) + '% (' + ctrl.uploaded() + '/' + ctrl.uploadTotal() + ' uploaded)') : null,
+		}, ' Send files'),
+		' ',
+		ctrl.uploaded() ? m('span',  Math.trunc(ctrl.uploaded() / ctrl.uploadTotal() * 100) + '% (' + ctrl.uploaded() + '/' + ctrl.uploadTotal() + ' uploaded)') : null,
+		' ',
 		m('input', {
 			style: {
 				display: 'inline'
 			},
 			type: 'file',
+			multiple: true,
 			config: ctrl.fileInput,
 			onchange: ctrl.fileChange
 		})
