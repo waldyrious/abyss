23be10ed93d4903a7292bcf97073812f3192413d
llambda <xxgsoftware@gmail.com>
2015-07-20 14:10:16 -0500

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,6 @@
-self.addEventListener('push', function (event) {
+// https://developers.google.com/web/updates/2015/03/push-notificatons-on-the-open-web
+
+self.addEventListener('push', function(event) {
 	console.log('Received a push message', event);
 
 	var title = 'Yobro.net';
@@ -6,7 +8,9 @@ self.addEventListener('push', function (event) {
 	//  var icon = '/images/icon-192x192.png';
 	var tag = 'simple-push-demo-notification-tag';
 
-	clients.matchAll({includeUncontrolled: true}).then(function (clients) {
+	clients.matchAll({
+		includeUncontrolled: true
+	}).then(function(clients) {
 		console.log('outer clients');
 		console.log(clients)
 		windowclients = clients;
@@ -32,4 +36,29 @@ self.addEventListener('push', function (event) {
 		//      icon: icon,
 		tag: tag
 	})));
+});
+
+self.addEventListener('notificationclick', function(event) {
+	console.log('On notification click: ', event.notification.tag);
+	// Android doesn't close the notification when you click on it
+	// See: http://crbug.com/463146
+	event.notification.close();
+
+	// This looks to see if the current window is already open and
+	// focuses if it is
+	event.waitUntil(
+		clients.matchAll({
+			type: "window"
+		})
+		.then(function(clientList) {
+			for (var i = 0; i < clientList.length; i++) {
+				var client = clientList[i];
+				if (client.url == '/' && 'focus' in client)
+					return client.focus();
+			}
+			if (clients.openWindow) {
+				return clients.openWindow('/');
+			}
+		})
+	);
 });
