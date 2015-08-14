0597514c8976b73193e6c4566a302119cc3843a4
llambda <xxgsoftware@gmail.com>
2015-08-13 23:45:11 -0500

    Randomize radio station

--- app/radio.js
+++ app/radio.js
@@ -1,71 +1,83 @@
 'use strict';
 var m = require('mithril');
 
+var randomProperty = function (obj) {
+    var keys = Object.keys(obj)
+    return keys[ keys.length * Math.random() << 0];
+};
+
 var audioSources = {
-    'Trap': 'http://208.113.211.151:443/trap/;',
-    'Chillstep': 'http://208.113.211.151:443/chillstep/;',
-    'Progressive House': 'http://208.113.211.151:443/prog-house/;',
-    'Dubstep': 'http://208.113.211.151:443/dubstep/;',
-    'Pop': 'http://208.113.211.151:443/getpsyched/;'
+	'Trap': 'http://208.113.211.151:443/trap/;',
+	'Chillstep': 'http://208.113.211.151:443/chillstep/;',
+	'Progressive House': 'http://208.113.211.151:443/prog-house/;',
+	'Dubstep': 'http://208.113.211.151:443/dubstep/;',
+	'Pop': 'http://208.113.211.151:443/getpsyched/;'
 }
-var audioKey = 'Trap';
+// var audioKey = 'Trap';
+var audioKey = randomProperty(audioSources);
 var element = new Audio(audioSources[audioKey]);
 element.preload = 'none';
 
 var playing = false;
 
 function stop() {
-    element.pause();
-    element.preload = 'none';
-    element.src = '';
-    playing = false;
+	element.pause();
+	element.preload = 'none';
+	element.src = '';
+	playing = false;
 }
 
 function play() {
-    element.src = audioSources[audioKey];
-    element.load();
-    element.play();
-    playing = true;
+	element.src = audioSources[audioKey];
+	element.load();
+	element.play();
+	playing = true;
 }
 
 function toggle() {
-    if (playing) stop()
-    else play();
+	if (playing) stop()
+	else play();
 }
 
 
 module.exports.controller = function(args, extras) {
-    var self = this;
+	var self = this;
 
-    self.play = function (ev) {
-        element.play();
-    }
+	self.play = function(ev) {
+		element.play();
+	}
 
-	self.changeStation = function (key) {
-        audioKey = key;
-        play();
+	self.changeStation = function(key) {
+		audioKey = key;
+		play();
 	}
 }
 
 module.exports.view = function(ctrl, args, extras) {
-    return m('span', [
-        m('button.btn btn-default glyphicon', { // needed for mobile safari
-            class: playing ? 'glyphicon-pause' : 'glyphicon-play',
-            style: {
-                float: 'right'
-            },
-            onclick: toggle
-        }
-        // , playing ? ' Pause' : ' Play'
-    ),
-        m('select', {
-            style: {
-                float: 'right'
-            },
-            value: audioKey,
-            onchange : function() { ctrl.changeStation(this.options[this.selectedIndex].value) },
-        }, Object.keys(audioSources).map(function (item) {
-            return m('option', {value: item}, item)
-        }))
-    ])
+	return m('span', [
+		m('button.btn btn-default glyphicon', { // needed for mobile safari
+				class: playing ? 'glyphicon-pause' : 'glyphicon-play',
+				style: {
+					float: 'right',
+					'margin-right': '16em'
+				},
+				onclick: toggle
+			}
+			// , playing ? ' Pause' : ' Play'
+		),
+		m('select', {
+			style: {
+				float: 'right'
+			},
+			value: audioKey,
+			onchange: function() {
+				ctrl.changeStation(this.options[this.selectedIndex].value)
+			},
+		}, Object.keys(audioSources).map(function(item) {
+			return m('option', {
+				value: item,
+                // selected: item == audioKey
+			}, item)
+		}))
+	])
 }
