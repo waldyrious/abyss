08b7bb1527392165bd6a4240bd629934de28b10c
Grant Miner <xxgsoftware@gmail.com>
2015-09-01 23:58:34 -0500

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,10 @@ rm -rf dist
 mkdir dist
 mkdir dist/client
 ./node_modules/.bin/webpack --optimize-minimize --optimize-minimize
-cp -R proxy.js lib package.json server.js model schema.js dist &&
+cp -R proxy.js lib package.json web.js webtls.js model schema.js dist &&
 cp app/index.html client &&
 cp app/sw.js client &&
 cp app/manifest.json client &&
 cp -R client dist &&
+cp processes.json-prod dist &&
 scp -r dist/* joe@104.237.135.61:yobro
