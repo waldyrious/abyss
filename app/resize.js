298cec66e678d3c82096ce5d01d3285797142306
Grant Miner <xxgsoftware@gmail.com>
2015-09-07 13:14:27 -0500

    style fixes

--- app/resize.js
+++ app/resize.js
@@ -4,6 +4,8 @@ var nav;
 var left;
 var right;
 
+var body = document.getElementsByTagName('body')[0];
+
 module.exports.registerNav = function (theNav) {
     nav = theNav;
     resize();
@@ -22,25 +24,32 @@ function resize(ev) {
     var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
     var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
 
-    if (w < 768) {
-        document.body.style.background = 'black';
+    if (w > 768) {
+        body.style.background = "inherit";
+        body.style.backgroundSize = "cover";
+        body.style.backgroundImage = "url('img/dark.jpg')";
+        body.style.backgroundRepeat = "none";
+    } else {
+        body.style.backgroundImage = "none";
+        body.style.backgroundColor = "black";
     }
 
-    if (nav && left && right) {
+    if (nav && left && right && w > 768) {
         var navHeight = nav.clientHeight;
         var newHeight = h - navHeight + "px";
-        if (w > 768) {
-            left.style.height = newHeight;
-            left.style.overflowY = 'scroll';
-            right.style.height = newHeight;
-            right.style.overflowY = 'scroll';
-        } else {
+
+        left.style.height = newHeight;
+        right.style.height = newHeight;
+        left.style.overflowY = 'scroll';
+        right.style.overflowY = 'scroll';
+    } else {
+        if (left) {
             left.style.height = '100%';
+        }
+        if (right) {
             right.style.height = '100%';
         }
     }
 }
-
+resize();
 window.onresize = resize;
-
-// document.documentElement.style.backgroundImage = require('./img/intro-bg.jpg');
