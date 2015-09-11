524990889197ccfab6c340a022eedb73701ca003
Grant Miner <xxgsoftware@gmail.com>
2015-09-10 23:06:17 -0500

    Oopsies

--- app/messages.js
+++ app/messages.js
@@ -44,7 +44,7 @@ function renderPhoneNumber(p) {
 	if (p.length !== 10)
 		return p;
 
-	return '(' + p[0]+p[1]+p[2] + ') ' + p[3]+p[4]+[5] + '-' + p[6]+p[7]+p[8]+p[9];
+	return '(' + p[0]+p[1]+p[2] + ') ' + p[3]+p[4]+p[5] + '-' + p[6]+p[7]+p[8]+p[9];
 }
 
 module.exports.controller = function(args, extras) {
@@ -699,7 +699,8 @@ module.exports.view = function(ctrl, args, extras) {
 							},
 							class: isEqual(flatten(grouping.group), ctrl.to) ? 'btn-success' : 'btn-default'
 						}, [simplify(grouping.group).map(function(ph) {
-							return m('div', (ctrl.getNickname(ph) ? ctrl.getNickname(ph) + ' ' : '') + renderPhoneNumber(ph))
+							// return m('div', (ctrl.getNickname(ph) ? ctrl.getNickname(ph) + ' ' : '') + renderPhoneNumber(ph))
+							return m('div', ctrl.getNickname(ph) ? ctrl.getNickname(ph) : renderPhoneNumber(ph))
 						})]),
 						m('br')
 					]
