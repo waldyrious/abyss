dd3fd62b916f451dc6cf7f261bf4063743850303
Grant Miner <xxgsoftware@gmail.com>
2015-11-24 20:18:57 -0600

    Remove proxy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ rm -rf dist
 mkdir dist
 mkdir dist/client
 ./node_modules/.bin/webpack --optimize-minimize --optimize-minimize
-cp -R proxy.js lib package.json web.js webtls.js model schema.js dist &&
+cp -R lib package.json web.js webtls.js model schema.js dist &&
 cp app/index.html client &&
 cp -R app/favicon/* client &&
 cp -R app/img client/img &&
